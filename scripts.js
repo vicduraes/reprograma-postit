bef9bc12c8e16015b84a44f898bf11bd0b6c99d0
vicduraes <duraes.vic@gmail.com>
2018-10-23 20:59:13 -0300

    Refatorar Página Login e passar valores variáveis para os componentes

--- scripts.js
+++ scripts.js
@@ -1,9 +1,23 @@
-const linkCriarConta = <a class="link" href="/conta">Criar uma conta</a>
-const linkLogin = <a class="link" href="/login">Fazer login</a>
-const formulario = (
-    <form>
-        {[linkCriarConta, linkLogin]}
-    </form>
+function Link (props){
+    return (
+        <a className="link" href={props.url}>
+            {props.children}
+        </a>
+    )
+}
+
+function Botao(props){
+    return <input type="submit" className="botao" value={props.value}/>
+}
+
+const paginaLogin = (
+  <main className="pagina-login">
+    <h1>Login</h1>
+    <p>Entre com seu email e senha</p>
+    <Link url="/conta">Criar uma conta</Link>
+    <Botao value="Enviar"/>
+  </main>  
 )
+
 const divisaoProjeto = document.querySelector("#projeto");
-ReactDOM.render(formulario, divisaoProjeto);
+ReactDOM.render(paginaLogin, divisaoProjeto);
