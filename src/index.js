a35bc26bdbe758fb2f01949d8fe7123ce7ed7be1
vicduraes <duraes.vic@gmail.com>
2018-10-23 22:28:44 -0300

    Usar Node modules

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import './index.css'
+
+
 function Link (props){
     return (
         <a className="link" href={props.url}>
@@ -7,7 +12,11 @@ function Link (props){
 }
 
 function Botao(props){
-    return <input type="submit" className="botao" value={props.value}/>
+    let classes = "botao"
+    if (props.desabilitado){
+        classes += " botao--desabilitado"
+    }
+    return <input type="submit" className={classes} value={props.value}/>
 }
 
 const paginaLogin = (
@@ -15,7 +24,9 @@ const paginaLogin = (
     <h1>Login</h1>
     <p>Entre com seu email e senha</p>
     <Link url="/conta">Criar uma conta</Link>
+    <Link url="/login">Fazer Login</Link>
     <Botao value="Enviar"/>
+    <Botao desabilitado value="Enviar"/>
   </main>  
 )
 
