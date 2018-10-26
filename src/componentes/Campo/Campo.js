a34ff5f0a7dc631168bf524d85ca824bcda4589c
vicduraes <duraes.vic@gmail.com>
2018-10-25 22:29:58 -0300

    Colocar class no Campo refatorar o código

--- src/componentes/Campo/Campo.js
+++ src/componentes/Campo/Campo.js
@@ -1,10 +1,40 @@
 import React from "react";
 import "./Campo.css";
 
-function Campo (props){
-  return (
-    <input id={props.id} className="caixa-texto" type={props.type} name={props.name} placeholder={props.placeholder}/>
-  )
+class Campo extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      erro: ''
+    }
+  }
+
+  valida = (evento) => {
+    const alvo = evento.target;
+
+    if (this.props.obrigatorio && alvo.value.trim() === '') {
+      const state = {
+        erro: 'Campo obrigatório'
+      }
+      this.setState(state)
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          id={this.props.id}
+          className="caixa-texto"
+          type={this.props.type}
+          name={this.props.name}
+          placeholder={this.props.placeholder}
+          onChange={this.valida}
+        />
+        <p className="grupo__erro">{this.state.erro}</p>
+      </div>
+    )
+  }
 }
 
 export default Campo;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import './index.css';
 import Login from "./paginas/Login/Login";
+import Conta from "./paginas/Conta/Conta";
 
 const divisaoProjeto = document.querySelector("#projeto");
-ReactDOM.render(<Login />, divisaoProjeto);
+ReactDOM.render(<Conta />, divisaoProjeto);
