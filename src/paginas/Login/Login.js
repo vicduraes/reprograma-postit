cef8fb7f92584a9db523292c8349d95aeaadfb1c
vicduraes <duraes.vic@gmail.com>
2018-10-26 22:06:28 -0300

    add required e pattern nos campos das pags

--- src/paginas/Login/Login.js
+++ src/paginas/Login/Login.js
@@ -11,9 +11,9 @@ function Login() {
     <h1>Login</h1>
     <p>Entre com seu email e senha</p>
     <Legenda htmlFor="email">Email:</Legenda>
-    <Campo id="email" type="email" name="email" placeholder="Email" required />
+    <Campo id="email" type="email" name="email" placeholder="Email" required minLength={6}/>
     <Legenda htmlFor="senha">Senha:</Legenda>
-    <Campo id="senha" type="password" name="senha" placeholder="Senha" required/>
+    <Campo id="senha" type="password" name="senha" placeholder="Senha" required pattern={/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/}/>
     <Link url="/conta">Criar uma conta</Link>
     <Link url="/login">Fazer Login</Link>
     <Botao value="Enviar" />
