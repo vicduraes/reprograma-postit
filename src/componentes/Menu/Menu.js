c9aa9991867aa40711ad9821e2c1aea9000f740e
vicduraes <duraes.vic@gmail.com>
2018-11-01 20:02:51 -0300

    Criar Componente Menu

--- src/componentes/Menu/Menu.js
+++ src/componentes/Menu/Menu.js
@@ -1,14 +1,32 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 import "./Menu.css";
 
 class Menu extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state = {aberto: false}
+        this.state = { aberto: false }
     }
 
-    render (){
+    render() {
+        return (
+            <nav className="navbar-menu">
+                <a className="navbar-menu__botao"> Menu </a>
+                <ul className="navbar-menu__opcoes">
+                    <li>
+                        <NavLink to="/quem-somos" activeClassName="navbar-menu__opcoes--ativo">Quem somos</NavLink>
+                    </li>
 
+                    <li>
+                        <NavLink to="/contato" activeClassName="navbar-menu__opcoes--ativo">Contato</NavLink>
+                    </li>
+
+                    <li>
+                        <NavLink to="/login" activeClassName="navbar-menu__opcoes--ativo">Sair</NavLink>
+                    </li>
+                </ul>
+            </nav>
+        )
     }
 }
 
--- src/componentes/Navbar/Navbar.js
+++ src/componentes/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Navbar.css";
 import logo from "./logo.png";
+import Menu from "../Menu/Menu";
 
 function Navbar() {
     return (
@@ -8,7 +9,7 @@ function Navbar() {
             <a href="/">
                 <img className="navbar__logo" src={logo} alt="logo" />
             </a>
-
+            <Menu/>
         </header>
     )
 }
