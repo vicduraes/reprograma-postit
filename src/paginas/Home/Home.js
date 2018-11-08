070fe3b6b1939e42917cbc36462c1c1cbf568cf0
vicduraes <duraes.vic@gmail.com>
2018-11-07 22:23:23 -0200

    Adicionar imagem de carregando na tela de postits

--- src/paginas/Home/Home.js
+++ src/paginas/Home/Home.js
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Redirect } from "react-router-dom";
+import carregando from "./carregando.svg";
 import "./Home.css";
 
-function Home(props) {
-    if (!props.usuaio) {
-        return <Redirect to="/login" />
-    }
-    return (
-        <main className="home">
+class Home extends Component {
 
-        </main>
-    )
+    constructor(props) {
+        super(props);
+        this.state = { carregando: true }
+    }
+
+    render() {
+        if (!this.props.usuaio) {
+            return <Redirect to="/login" />
+        }
+        return (
+            <main className="home">
+                {this.state.carregando ? (
+                    <img className="home__loading" src={carregando} alt="Carregando" />
+                ) : (
+                        <div>
+                            Nesse espaço listarei os postits amanhã
+                    </div>
+                    )}
+            </main>
+        )
+    }
 }
 
 // const state ={
